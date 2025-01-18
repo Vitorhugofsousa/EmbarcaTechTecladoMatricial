8f522077c7ef1f6884c22b375edee9b61a40151f
Guilherme <202311190034@ifba.edu.br>
2025-01-18 01:16:41 -0300

    Correção de Duplicação do Código

--- Main.c
+++ Main.c
@@ -76,13 +76,6 @@ void acionar_buzzer()
   gpio_put(gpio_buzzer, 0); // Desliga o buzzer
 }
 
-void acionar_buzzer()
-{
-  gpio_put(gpio_buzzer, 1); // Liga o buzzer
-  sleep_ms(2000);           // Emite som por 2000ms
-  gpio_put(gpio_buzzer, 0); // Desliga o buzzer
-}
-
 int main()
 {
   stdio_init_all();
@@ -127,53 +120,5 @@ int main()
       }
       sleep_ms(100); // Delay para evitar leitura repetida
     }
-  }
-}
-
-int main()
-{
-  stdio_init_all();
-  gpio_init(gpio_led_red);
-  gpio_init(gpio_led_green);
-  gpio_init(gpio_led_blue);
-  gpio_init(gpio_buzzer);
-  gpio_set_dir(gpio_led_red, GPIO_OUT);
-  gpio_set_dir(gpio_led_green, GPIO_OUT);
-  gpio_set_dir(gpio_led_blue, GPIO_OUT);
-  gpio_set_dir(gpio_buzzer, GPIO_OUT);
-
-  inicializar_teclado(coluna, linha);
-
-  while (true)
-  {
-    char tecla_pressionada = ler_teclado(coluna, linha); // Atribui a variável a tecla pressionada ao chamar a função
-
-    if (tecla_pressionada)
-    {
-      printf("Tecla pressionada: %c\n", tecla_pressionada);
-
-      switch (tecla_pressionada)
-      { // Analisa a tecla pressionada e executa a ação definido a ela
-      case 'A':
-        piscar_led(gpio_led_blue); // Verifica se a tecla A foi pressionada
-        break;
-
-      case 'B':                   // Verifica se a tecla B foi pressionada
-        piscar_led(gpio_led_red); // Chama a funcao piscar_periferico
-        break;
-
-      case 'D': // Piscar luz branca uma vez
-        pisca_led_branco();
-        break;
-      case '#': // Verifica se a tecla # foi pressionada
-        acionar_buzzer();
-        break;
-
-      default:
-        printf("Tecla não configurada\n");
-      }
-      
-    }
-    sleep_ms(100); // Delay para evitar leitura repetida  
   }
 }
