09814e6fe9c3c0bc55a2da97455a36e5b65e36ea
MariaP-Ramalho <eduardapramalho@gmail.com>
2025-01-18 21:05:25 -0300

    debug: ler teclado

--- Main.c
+++ Main.c
@@ -32,15 +32,12 @@ void inicializar_teclado() {
 char ler_teclado(uint8_t *colunas, uint8_t *linhas) {
   for (int i = 0; i < 4; i++) {
     gpio_put(linhas[i], 0);
-    uint8_t result = 0;
-    for (int j = 0; j < 4; j++){
-      result |= gpio_get(colunas[j]);
-    }
-    if (result == 0) {
-      char key = teclas[i][__builtin_ctz(result)];
-      gpio_put(linhas[i], 1);
-      return key;
-    }
+    for (int j = 0; j < 4; j++) {
+            if (!gpio_get(colunas[j])) { // Verifica se a coluna está LOW
+                gpio_put(linhas[i], 1);  // Restaura a linha
+                return teclas[i][j];    // Retorna a tecla correspondente
+            }
+        }
     gpio_put(linhas[i], 1);
   }
   return 0;
